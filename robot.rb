1b804fed7ea8e3d01791908930657f821218a805
klocode <kendrick.t.lo@gmail.com>
2017-02-01 21:53:12 -0500

    finishing touches on assignment for the last difficulties

--- robot.rb
+++ robot.rb
@@ -15,6 +15,10 @@ class Robot
     puts "My name is #{name}!"
   end
 
+  def method_missing(m, *args, &block)
+    puts "Does Not Compute: There's no method called #{m} here -- please try again."
+  end
+
 end
 
 bot = Robot.new("Bot")
@@ -39,25 +43,36 @@ class ActorUnit < Robot
   end
 end
 
-jack = ActorUnit.new("Jack")
+kendrick = ActorUnit.new("Kendrick")
+kendrick.flip #testing if I call an undefined method
 
-jack.change_name("Jill")
+kendrick.change_name("Ken")
 
-# puts jack.inspect
+########################ADVENTURE##############################
 
 our_class = %w(Kalea Nancy Dave Rob Laura David Phil Ben Kendrick Demetra Dave Laura Miguel Michael)
 
 robots = []
 
+#using each method to convert students to robots
 our_class.each do |robo|
   robots << Robot.new(robo)
 end
 
+#making it look pretty because I do not know another way to make the output nice
 robots.each do |robot|
   puts robot.inspect
 end
 
-
+#using enumerator to turn students into robots
 student_robot = our_class.map {|bott| Robot.new(bott)}
 
 puts student_robot.inspect
+
+###########################EPIC#################################
+
+robotclasses = [Robot, BendingUnit, ActorUnit]
+
+changed_robot = our_class.map { |stu| robotclasses.sample.new(stu) }
+
+puts changed_robot.inspect
